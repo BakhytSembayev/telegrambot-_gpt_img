68cbba3ea2a9a261ee2301ff31b24ebcf80e4ded
BakhytSembayev <119586792+BakhytSembayev@users.noreply.github.com>
2023-04-28 15:38:04 +0600

    Add files via upload

---
+++ telegrambot-master/backend/modules/telegram/services/sTelegram.php
@@ -0,0 +1,447 @@
+<?php
+/**
+ * VladimirGav
+ * GitHub Website: https://vladimirgav.github.io/
+ * GitHub: https://github.com/VladimirGav
+ * Copyright (c)
+ */
+
+namespace modules\telegram\services;
+
+class sTelegram
+{
+
+    public $telegramApi = 'https://api.telegram.org/bot';
+
+    /**
+     * @var
+     */
+    protected static $instance;
+
+    public static function instance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+
+    /**
+     * Отправить сообщение
+     * @param $bot_token
+     * @param $chat_id
+     * @param $text
+     * @return array
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
+     */
+    public function sendMessage($bot_token, $chat_id, $text, $reply_markup='', $reply_to_message_id=''){
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $dataMessage=[];
+        $dataMessage['chat_id']=$chat_id;
+        $dataMessage['text']=$text;
+        if(!empty($reply_markup)){
+            $dataMessage['reply_markup']=$reply_markup;
+        }
+        if(!empty($reply_to_message_id)){
+            $dataMessage['reply_to_message_id']=$reply_to_message_id;
+        }
+        $dataMessage['parse_mode']='HTML';
+        if($dataMessage['parse_mode']=='HTML'){
+            $dataMessage['text'] = str_replace("<br>", PHP_EOL, $dataMessage['text']);
+            $dataMessage['text'] = str_replace("<br />", PHP_EOL, $dataMessage['text']);
+            $dataMessage['text'] = strip_tags($dataMessage['text'], '<b><strong><i><em><u><ins><s><strike><del><span><tg-spoiler><b><a><code><pre>');
+        }
+        $dataMessage['disable_web_page_preview'] = true;
+
+        $getMessageId=0;
+        $error=0;
+        $dataText='';
+
+        try {
+            $sendMessageData = $telegram->sendMessage($dataMessage);
+            $getMessageId = $sendMessageData->getMessageId();
+            $dataText = 'Успешно';
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+
+        return ['error' => $error, 'data' => $dataText, 'MessageId'=>$getMessageId];
+    }
+
+    public function removeMessage($bot_token, $chat_id, $message_id)
+    {
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $error=0;
+        $dataText = 'Успешно';
+
+        try {
+            $deleteMessageData = $telegram->deleteMessage(['chat_id'=>$chat_id, 'message_id'=> $message_id]);
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+        return ['error' => $error, 'data' => $dataText];
+    }
+
+    /**
+     * Устанавливаем связь с ботом и задаем url api
+     * @param $bot_token
+     * @param $website_url
+     * @return false|string
+     */
+    public function setWebhook($bot_token, $website_url){
+        ob_start();
+        echo file_get_contents($this->telegramApi.$bot_token.'/setWebhook?url='.$website_url);
+        $contents = ob_get_contents();             //  Instead, output above is saved to $contents
+        ob_end_clean();
+        return $contents;
+    }
+
+    public function getWebhookUpdate($bot_token){
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $dataMessage = $telegram->getWebhookUpdate();
+        return $dataMessage;
+    }
+
+    public function checkApi($bot_token){
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        try {
+            $response = $telegram->getMe();
+        } catch (\Exception $e) {
+            return ['error'=>1, 'data' => 'Telegram Bot API Token error'];
+        }
+        return ['error'=>0, 'data' => $response];
+    }
+
+    /**
+     * Use this method to remove a previously set outgoing webhook.
+     * @param $bot_token
+     * @return \Telegram\Bot\TelegramResponse
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
+     */
+    public function removeWebhook($bot_token){
+        //exit;
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        try {
+            $response = $telegram->removeWebhook();
+        } catch (\Exception $e) {
+            return ['error'=>1, 'data' => 'Telegram Bot API Token error'];
+        }
+        return ['error'=>0, 'data' => $response];
+    }
+
+
+    /**
+     * Получаем последнее сообщение избегая повторов
+     * @param $bot_token
+     * @return array|\Telegram\Bot\Objects\Update
+     */
+    public function getWebhookLastMessage($bot_token){
+        $LatestMessage=[];
+        $lastUpdateId = $this->getLastUpdateId($bot_token);
+        $dataMessage = $this->getWebhookUpdate($bot_token);
+
+        if(!empty($dataMessage['update_id']) && $dataMessage['update_id'] != $lastUpdateId){
+            $this->saveLastUpdateId($bot_token, $dataMessage['update_id']);
+            $LatestMessage = $dataMessage;
+        }
+        return $LatestMessage;
+    }
+
+    /**
+     * Получаем последнее сообщение избегая повторов
+     * @param $bot_token
+     * @return array|mixed
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
+     */
+    public function getUpdatesLastMessage($bot_token){
+        $LatestMessage=[];
+        $params=[];
+
+        $lastUpdateId = $this->getLastUpdateId($bot_token);
+        $params['offset']=$lastUpdateId;
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        try {
+            $UpdatesData = $telegram->getUpdates($params);
+        } catch (Exception $e) {
+            return ['error'=>1, 'data' => 'Telegram Bot API Token error'];
+        }
+        $UpdatesDataArr = [];
+        if(!empty($UpdatesData) && is_array($UpdatesData)){
+            foreach ($UpdatesData as $UpdateData){
+                $UpdatesDataArr[]=$UpdateData->toArray();
+            }
+        }
+
+        $nextid=($lastUpdateId>0)?1:0;
+        if(!empty($UpdatesDataArr[$nextid]['update_id'])){
+            $this->saveLastUpdateId($bot_token, $UpdatesDataArr[$nextid]['update_id']);
+            $LatestMessage = $UpdatesDataArr[$nextid];
+        }
+        return $LatestMessage;
+    }
+
+    public function getLastUpdateId($bot_token){
+        $FileLastUpdateId = $this->getFileLastUpdateId($bot_token);
+        $lastUpdateId = 0;
+        if(file_exists($FileLastUpdateId)){
+            $lastUpdateId = file_get_contents($FileLastUpdateId);
+        }
+        return $lastUpdateId;
+    }
+
+    public function saveLastUpdateId($bot_token, $lastUpdateId){
+        $FileLastUpdateId = $this->getFileLastUpdateId($bot_token);
+        $dirFile = dirname($FileLastUpdateId);
+
+        if (!is_dir($dirFile)) { if (!@mkdir($dirFile, 0755, true)) { return false; } }
+        file_put_contents($FileLastUpdateId, $lastUpdateId);
+        return true;
+    }
+
+    public function getFileLastUpdateId($bot_token){
+        $botName = substr($bot_token,-6);
+        return __DIR__.'/telegram-ids/last-id-'.$botName.'.txt';
+    }
+
+    /**
+     * A simple method for testing your bot's auth token.
+     * @param $bot_token
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
+     */
+    public function getMe($bot_token){
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $response = $telegram->getMe();
+        $botId = $response->getId();
+        $firstName = $response->getFirstName();
+        $username = $response->getUsername();
+    }
+
+    public function forwardMessage($bot_token, $chat_id, $from_chat_id, $message_id)
+    {
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $response = $telegram->forwardMessage([
+            'chat_id' => $chat_id,
+            'from_chat_id' => $from_chat_id,
+            'message_id' => $message_id
+        ]);
+
+        $messageId = $response->getMessageId();
+    }
+
+    public function sendPhoto($bot_token, $chat_id, $file, $caption='', $reply_to_message_id='')
+    {
+        if(!file_exists($file)){
+            return ['error' => 1, 'data' => 'File not found'];
+        }
+        $InputFile = \Telegram\Bot\FileUpload\InputFile::create($file);
+        $telegram = new \Telegram\Bot\Api($bot_token);
+
+        $dataMessage=[];
+        $dataMessage['chat_id']=$chat_id;
+        $dataMessage['photo']=$InputFile;
+        if(!empty($caption)){ $dataMessage['caption']=$caption; }
+        if(!empty($reply_to_message_id)){ $dataMessage['reply_to_message_id']=$reply_to_message_id; }
+
+        $getMessageId=0;
+        $error=0;
+        try {
+            $sendMessageData = $telegram->sendPhoto($dataMessage);
+            $getMessageId = $sendMessageData->getMessageId();
+            $dataText = 'Успешно';
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+        return ['error' => $error, 'data' => $dataText, 'MessageId'=>$getMessageId];
+    }
+
+    public function sendAudio($bot_token, $chat_id, $file, $caption='', $reply_to_message_id='')
+    {
+        if(!file_exists($file)){
+            return ['error' => 1, 'data' => 'File not found'];
+        }
+        $InputFile = \Telegram\Bot\FileUpload\InputFile::create($file);
+        $telegram = new \Telegram\Bot\Api($bot_token);
+
+        $dataMessage=[];
+        $dataMessage['chat_id']=$chat_id;
+        $dataMessage['audio']=$InputFile;
+        if(!empty($caption)){ $dataMessage['caption']=$caption; }
+        if(!empty($reply_to_message_id)){ $dataMessage['reply_to_message_id']=$reply_to_message_id; }
+
+        $getMessageId=0;
+        $error=0;
+        try {
+            $sendMessageData = $telegram->sendAudio($dataMessage);
+            $getMessageId = $sendMessageData->getMessageId();
+            $dataText = 'Успешно';
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+        return ['error' => $error, 'data' => $dataText, 'MessageId'=>$getMessageId];
+    }
+
+    public function sendDocument($bot_token, $chat_id, $file, $caption='', $reply_to_message_id='')
+    {
+        if(!file_exists($file)){
+            return ['error' => 1, 'data' => 'File not found'];
+        }
+        $InputFile = \Telegram\Bot\FileUpload\InputFile::create($file);
+        $telegram = new \Telegram\Bot\Api($bot_token);
+
+        $dataMessage=[];
+        $dataMessage['chat_id']=$chat_id;
+        $dataMessage['document']=$InputFile;
+        if(!empty($caption)){ $dataMessage['caption']=$caption; }
+        if(!empty($reply_to_message_id)){ $dataMessage['reply_to_message_id']=$reply_to_message_id; }
+
+        $getMessageId=0;
+        $error=0;
+        try {
+            $sendMessageData = $telegram->sendDocument($dataMessage);
+            $getMessageId = $sendMessageData->getMessageId();
+            $dataText = 'Успешно';
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+        return ['error' => $error, 'data' => $dataText, 'MessageId'=>$getMessageId];
+    }
+
+    public function sendVideo($bot_token, $chat_id, $file, $caption='', $reply_to_message_id='')
+    {
+        if(!file_exists($file)){
+            return ['error' => 1, 'data' => 'File not found'];
+        }
+        $InputFile = \Telegram\Bot\FileUpload\InputFile::create($file);
+        $telegram = new \Telegram\Bot\Api($bot_token);
+
+        $dataMessage=[];
+        $dataMessage['chat_id']=$chat_id;
+        $dataMessage['video']=$InputFile;
+        if(!empty($caption)){ $dataMessage['caption']=$caption; }
+        if(!empty($reply_to_message_id)){ $dataMessage['reply_to_message_id']=$reply_to_message_id; }
+
+        $getMessageId=0;
+        $error=0;
+        try {
+            $sendMessageData = $telegram->sendVideo($dataMessage);
+            $getMessageId = $sendMessageData->getMessageId();
+            $dataText = 'Успешно';
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+        return ['error' => $error, 'data' => $dataText, 'MessageId'=>$getMessageId];
+    }
+
+    public function sendVoice($bot_token, $chat_id, $file, $caption='', $reply_to_message_id='')
+    {
+        if(!file_exists($file)){
+            return ['error' => 1, 'data' => 'File not found'];
+        }
+        $InputFile = \Telegram\Bot\FileUpload\InputFile::create($file);
+        $telegram = new \Telegram\Bot\Api($bot_token);
+
+        $dataMessage=[];
+        $dataMessage['chat_id']=$chat_id;
+        $dataMessage['voice']=$InputFile;
+        if(!empty($caption)){ $dataMessage['caption']=$caption; }
+        if(!empty($reply_to_message_id)){ $dataMessage['reply_to_message_id']=$reply_to_message_id; }
+
+        $getMessageId=0;
+        $error=0;
+        try {
+            $sendMessageData = $telegram->sendVoice($dataMessage);
+            $getMessageId = $sendMessageData->getMessageId();
+            $dataText = 'Успешно';
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+        return ['error' => $error, 'data' => $dataText, 'MessageId'=>$getMessageId];
+    }
+
+    public function sendSticker($bot_token, $chat_id, $file, $reply_to_message_id='')
+    {
+        if(!file_exists($file)){
+            return ['error' => 1, 'data' => 'File not found'];
+        }
+        $InputFile = \Telegram\Bot\FileUpload\InputFile::create($file);
+        $telegram = new \Telegram\Bot\Api($bot_token);
+
+        $dataMessage=[];
+        $dataMessage['chat_id']=$chat_id;
+        $dataMessage['sticker']=$InputFile;
+        if(!empty($reply_to_message_id)){ $dataMessage['reply_to_message_id']=$reply_to_message_id; }
+
+        $getMessageId=0;
+        $error=0;
+        try {
+            $sendMessageData = $telegram->sendSticker($dataMessage);
+            $getMessageId = $sendMessageData->getMessageId();
+            $dataText = 'Успешно';
+        } catch (\Exception $e) {
+            $dataText = 'Exception: '.  $e->getMessage();
+            $error = 1;
+        }
+
+        return ['error' => $error, 'data' => $dataText, 'MessageId'=>$getMessageId];
+    }
+
+    public function sendLocation($bot_token)
+    {
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        // Sends San Francisco, CA Location.
+        $response = $telegram->sendLocation([
+            'chat_id' => 'CHAT_ID',
+            'latitude' => 37.7576793,
+            'longitude' => -122.5076402,
+        ]);
+
+        $messageId = $response->getMessageId();
+    }
+
+    public function sendChatAction($bot_token)
+    {
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $telegram->sendChatAction([
+            'chat_id' => 'CHAT_ID',
+            'action' => 'upload_photo'
+        ]);
+    }
+
+    public function getUserProfilePhotos($bot_token)
+    {
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $response = $telegram->getUserProfilePhotos(['user_id' => 'USER_ID']);
+
+        $photos_count = $response->getTotalCount();
+        $photos = $response->getPhotos();
+    }
+
+    public function getUpdates($bot_token)
+    {
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $response = $telegram->getUpdates();
+    }
+
+    public function getFile($bot_token)
+    {
+        $telegram = new \Telegram\Bot\Api($bot_token);
+        $response = $telegram->getFile(['file_id' => 'ABC12345XYZ6789...']);
+    }
+
+
+}
---
+++ telegrambot-master/www/examples/telegrambotapi.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * VladimirGav
+ * GitHub Website: https://vladimirgav.github.io/
+ * GitHub: https://github.com/VladimirGav
+ * Copyright (c)
+ */
+
+// Устанавливаем и подключаем Composer
+require_once __DIR__.'/../../backend/defines.php';
+
+/** Пример обработки сообщений телеграм бота */
+
+use modules\telegram\services\sTelegram;
+
+// Получим токен бота из файла
+if(!file_exists(_FILE_bot_token_)){
+    exit(_FILE_bot_token_.' is empty');
+}
+$bot_token = trim(file_get_contents(_FILE_bot_token_));
+
+// Подключаемся к апи
+$telegram = new \Telegram\Bot\Api($bot_token);
+
+/*$checkApi = sTelegram::instance()->checkApi($bot_token);
+if(!empty($checkApi['error'])){
+    echo json_encode($checkApi);
+    exit;
+}*/
+
+// Если запускаем через консоль, а не используем Telegram Webhook
+if(!empty($_SERVER['argv'][1]) && $_SERVER['argv'][1]=='console'){
+    $removeWebhook = sTelegram::instance()->removeWebhook($bot_token); // Удаляем привязку к Telegram Webhook
+    if(!empty($removeWebhook['error'])){ exit(json_encode($removeWebhook)); }
+    $dataMessage = sTelegram::instance()->getUpdatesLastMessage($bot_token);
+} else {
+    $dataMessage = sTelegram::instance()->getWebhookLastMessage($bot_token);
+}
+
+if(empty($dataMessage['message']['message_id'])){
+    //echo json_encode(['error'=> 1, 'data' => 'message_id empty']);
+    exit;
+}
+if(empty($dataMessage['message']['chat']['id'])){
+    echo json_encode(['error'=> 1, 'data' => 'chat_id empty']);
+    exit;
+}
+if(empty($dataMessage['message']['text'])){
+    echo json_encode(['error'=> 1, 'data' => 'text empty']);
+    exit;
+}
+
+// Получим данные от пользователя
+$message_id = $dataMessage['message']['message_id']; // Id сообщения
+$message_chat_id = $dataMessage['message']['chat']['id']; // Id чата
+$message_text = $dataMessage['message']['text']; // Текст сообщения
+
+// К нижнему регистру
+$messageTextLower = mb_strtolower($message_text);
+
+// Удаляем имя бота, например заменяеам /ai@Name_bot на /ai
+$messageTextLower = preg_replace('/(.*)(\/ai@[^ ]*)(.*)/', '/ai $1$3', $messageTextLower);
+$messageTextLower = preg_replace('/(.*)(\/img@[^ ]*)(.*)/', '/img $1$3', $messageTextLower);
+
+// Если первое сообщение
+if($messageTextLower=='/start'){
+    sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Привет, я бот', '', $message_id);
+    exit;
+}
+
+// Если пользователь напишет Тест, то выведем ответ
+if($messageTextLower=='тест'){
+    sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Ответ от бота на сообщение тест. <b>Вы можете предусмотреть свои ответы на любые сообщения в формате HTML.</b>', '', $message_id);
+    exit;
+}
+
+// Если пользователь напишет привет
+if($messageTextLower=='привет'){
+    sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Привет', '', $message_id);
+    exit;
+}
+
+// пример ответа
+if($messageTextLower=='пример ответа'){
+    sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Ответ на сообщение', '', $message_id);
+    exit;
+}
+
+if($messageTextLower=='chat_id'){
+    sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'chat_id: '.$message_chat_id, '', $message_id);
+    exit;
+}
+
+// Пример отправки аудио файла
+if($messageTextLower=='мелодия'){
+    sTelegram::instance()->sendAudio($bot_token, $message_chat_id, __DIR__.'/audio.mp3', '', $message_id);
+    exit;
+}
+
+// пример кнопки
+if($messageTextLower=='пример кнопки'){
+    $inline_keyboard=[];
+    $inline_keyboard[][] = ["text"=>'telegram кнопка', "url"=>'https://telegram.org/'];
+    $keyboard=["inline_keyboard"=>$inline_keyboard];
+    $reply_markup = json_encode($keyboard);
+    sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Сообщение с кнопкой', $reply_markup);
+    exit;
+}
+
+// Пример chatGPT
+$pos2 = stripos($message_text, '/ai');
+if ($pos2 !== false) {
+    // Удаляем все лишнее
+    $message_text = trim($message_text);
+    $message_text = str_replace('/ai', '', $message_text);
+    $message_text = str_replace('  ', ' ', $message_text);
+    $message_text  = mb_strtolower($message_text);
+
+    // Если пустой, отправляем пример
+    if(empty($message_text)){
+        sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Example: /ai Ты можешь отвечать на вопросы?', '', $message_id);
+        exit;
+    }
+
+    // Получим токен бота из файла
+    if(!file_exists(_FILE_api_gpt_)){
+        sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'OpenAI API KEY is empty', '', $message_id);
+        exit;
+    }
+    $api_gpt = trim(file_get_contents(_FILE_api_gpt_));
+    if(empty($api_gpt)){
+        sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'OpenAI API KEY is empty', '', $message_id);
+        exit;
+    }
+
+    // gpt-3.5-turbo
+    $ChatGPTAnswerData = \modules\openai\services\sOpenAI::instance()->getChatGPTAnswer($api_gpt, $message_text);
+    if(!empty($ChatGPTAnswerData['error'])){
+        exit(json_encode($ChatGPTAnswerData));
+    }
+
+    if(!empty($ChatGPTAnswerData['answer'])){
+        sTelegram::instance()->sendMessage($bot_token, $message_chat_id, $ChatGPTAnswerData['answer'], '', $message_id);
+        exit;
+    }
+}
+
+// АИ Рисуем картинку по запросу
+$pos2 = stripos($message_text, '/img');
+if ($pos2 !== false) {
+    $dir = __DIR__.'/uploads/images';
+    if(!file_exists($dir)){
+        if (!mkdir($dir, 0777, true)) {
+            die('Не удалось создать директории...');
+        }
+    }
+
+    // Удаляем все лишнее
+    $message_text = trim($message_text);
+    $message_text = str_replace('/img', '', $message_text);
+    $message_text = str_replace('  ', ' ', $message_text);
+    $message_text  = mb_strtolower($message_text);
+
+    // Если пустой, отправляем пример
+    if(empty($message_text)){
+        sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Example: /img Рыжая лиса в лесу', '', $message_id);
+        exit;
+    }
+
+    // Получим токен бота из файла
+    if(!file_exists(_FILE_api_gpt_)){
+        sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'OpenAI API KEY is empty', '', $message_id);
+        exit;
+    }
+    $api_gpt = trim(file_get_contents(_FILE_api_gpt_));
+    if(empty($api_gpt)){
+        sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'OpenAI API KEY is empty', '', $message_id);
+        exit;
+    }
+
+    // gpt-3.5-turbo
+    $ImgData = \modules\openai\services\sOpenAI::instance()->getImg($api_gpt, $message_text);
+    if(!empty($ImgData['error'])){
+        exit(json_encode($ImgData));
+    }
+
+    if(!empty($ImgData['url'])){
+        // save img
+        $fileName = $dir.'/'.time().'.png';
+        file_put_contents($fileName, file_get_contents($ImgData['url']));
+
+        sTelegram::instance()->sendPhoto($bot_token, $message_chat_id, $fileName, $message_text, $message_id);
+        exit;
+    }
+
+}
+
+// Если не предусмотрен ответ
+sTelegram::instance()->sendMessage($bot_token, $message_chat_id, 'Ответ не предусмотрен', '', $message_id);
+exit;
+
